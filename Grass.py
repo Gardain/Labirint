f3e48be7c15f3c494c3315d5570cd73be8291865
ЙАЙЦУАП <ujl201@bk.ru>
2021-01-17 14:17:56 +0300

    flour

--- Grass.py
+++ Grass.py
@@ -2,14 +2,14 @@ from LevelGroup import LevelGroup
 from LevelSprite import LevelSprite
 
 
-class Grass(LevelSprite):
+class FLour(LevelSprite):
     def __init__(self, row, col, *groups):
         super().__init__(row, col, 'flour.png', groups)
 
 
-class GrassGroup(LevelGroup):
+class FlourGroup(LevelGroup):
     def __init__(self, level):
         super().__init__(level)
         self.key = ['.', '@']
-        self.sprite_class = Grass
+        self.sprite_class = Flour
         self.create_sprites()
