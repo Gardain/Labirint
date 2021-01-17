76accfcd51c4b7882712f73746be061bd5a0cf6e
ЙАЙЦУАП <ujl201@bk.ru>
2021-01-17 13:52:24 +0300

    Finish

--- Finish.py
+++ Finish.py
@@ -4,12 +4,12 @@ from LevelSprite import LevelSprite
 
 class Finish(LevelSprite):
     def __init__(self, row, col, *groups):
-        super().__init__(row, col, 'grass.png', groups)
+        super().__init__(row, col, 'finish.png', groups)
 
 
 class FinishGroup(LevelGroup):
     def __init__(self, level):
         super().__init__(level)
-        self.key = ['.']
-        self.sprite_class =
+        self.key = ['$']
+        self.sprite_class = Finish
         self.create_sprites()
