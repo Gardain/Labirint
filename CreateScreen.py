388c61ac588323f017877a5e4e9aae4841576cea
Gardain <ujl201@bk.ru>
2024-11-01 17:47:54 +0300

    Adaptation pictures for resizable screen

--- CreateScreen.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from game_config import config
-
-
-def create_screen():
-    width, height = config.get_list_values('width', 'height')
-    size = width, height
-    screen = pygame.display.set_mode(size)
-    return screen
